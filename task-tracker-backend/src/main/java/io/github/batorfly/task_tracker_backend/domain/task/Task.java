07763b9890866f1bf4957e465a99f6ae871fd17d
иликтуев атор рдэмович <batojjk@gmail.com>
2026-05-14 11:59:22 +0300

    skeleton for Task Entity-class

--- task-tracker-backend/src/main/java/io/github/batorfly/task_tracker_backend/domain/task/Task.java
+++ task-tracker-backend/src/main/java/io/github/batorfly/task_tracker_backend/domain/task/Task.java
@@ -1,5 +1,31 @@
 package io.github.batorfly.task_tracker_backend.domain.task;
 
-public class Task {
+import io.github.batorfly.task_tracker_backend.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor @AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Task {
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private User user;
+
+    private boolean isDone;
+
+    private Timestamp created;
+
+    private Timestamp updated;
+
+    private Timestamp completionTime;
 }
