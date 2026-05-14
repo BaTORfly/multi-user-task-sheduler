475b424d77b09dc78cd72e240fe0150ba497139d
иликтуев атор рдэмович <batojjk@gmail.com>
2026-05-14 13:53:32 +0300

    skeleton for User and Authority Entity-classes

--- task-tracker-backend/src/main/java/io/github/batorfly/task_tracker_backend/domain/user/Authority.java
+++ task-tracker-backend/src/main/java/io/github/batorfly/task_tracker_backend/domain/user/Authority.java
@@ -1,4 +1,36 @@
 package io.github.batorfly.task_tracker_backend.domain.user;
 
-public class Authority {
+import jakarta.persistence.*;
+import lombok.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "roles")
+@Builder @Setter
+@NoArgsConstructor @AllArgsConstructor
+@EqualsAndHashCode(of = {"id", "role"})
+public class Authority implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long id;
+
+    private User user;
+
+    private Roles role;
+
+    @Override
+    public @Nullable String getAuthority() {
+        return this.role.name();
+    }
+
+    public Authority(Roles role) {
+        this.role = role;
+    }
+
+    public enum Roles {
+        USER, ADMIN
+    }
 }
--- task-tracker-backend/src/main/java/io/github/batorfly/task_tracker_backend/domain/user/User.java
+++ task-tracker-backend/src/main/java/io/github/batorfly/task_tracker_backend/domain/user/User.java
@@ -1,5 +1,60 @@
 package io.github.batorfly.task_tracker_backend.domain.user;
 
-public class User {
+import io.github.batorfly.task_tracker_backend.domain.task.Task;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class User implements UserDetails {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String password;
+
+    private String email;
+
+    private boolean enabled = false;
+
+    private Set<Task> tasks = new HashSet<>();
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
