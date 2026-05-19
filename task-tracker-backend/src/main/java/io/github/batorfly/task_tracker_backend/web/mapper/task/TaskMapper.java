172033ebd5b89e988526617b6e613a09f40e3a9d
иликтуев атор рдэмович <batojjk@gmail.com>
2026-05-19 13:14:13 +0300

    realized mappers (TaskMapper and UserMapper) version 1.0

--- task-tracker-backend/src/main/java/io/github/batorfly/task_tracker_backend/web/mapper/task/TaskMapper.java
+++ task-tracker-backend/src/main/java/io/github/batorfly/task_tracker_backend/web/mapper/task/TaskMapper.java
@@ -4,29 +4,21 @@ import io.github.batorfly.task_tracker_backend.domain.task.Task;
 import io.github.batorfly.task_tracker_backend.web.dto.task.TaskDto;
 import io.github.batorfly.task_tracker_backend.web.mapper.Mappable;
 import org.mapstruct.Mapper;
-
-import java.util.List;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TaskMapper extends Mappable<Task, TaskDto> {
 
     @Override
-    default TaskDto toDto(Task entity) {
-        return null;
-    }
+    @Mapping(target = "isDone", source = "done")
+    @Mapping(target = "completionTime", source = "completionTime")
+    TaskDto toDto(Task entity);
 
     @Override
-    default List<TaskDto> toDto(List<Task> entities) {
-        return List.of();
-    }
-
-    @Override
-    default Task toEntity(TaskDto dto) {
-        return null;
-    }
-
-    @Override
-    default List<Task> toEntity(List<TaskDto> dtos) {
-        return List.of();
-    }
+    @Mapping(target = "done", source = "isDone")
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "createdTime", ignore = true)
+    @Mapping(target = "updatedTime", ignore = true)
+    @Mapping(target = "completionTime", ignore = true)
+    Task toEntity(TaskDto dto);
 }
--- task-tracker-backend/src/main/java/io/github/batorfly/task_tracker_backend/web/mapper/user/UserMapper.java
+++ task-tracker-backend/src/main/java/io/github/batorfly/task_tracker_backend/web/mapper/user/UserMapper.java
@@ -1,11 +1,53 @@
 package io.github.batorfly.task_tracker_backend.web.mapper.user;
 
+import io.github.batorfly.task_tracker_backend.domain.task.Task;
 import io.github.batorfly.task_tracker_backend.domain.user.User;
+import io.github.batorfly.task_tracker_backend.web.dto.task.TaskDto;
+import io.github.batorfly.task_tracker_backend.web.dto.user.UserWithTasksDto;
 import io.github.batorfly.task_tracker_backend.web.dto.user.UserWithoutTasksDto;
 import io.github.batorfly.task_tracker_backend.web.mapper.Mappable;
+import io.github.batorfly.task_tracker_backend.web.mapper.task.TaskMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
-public interface UserMapper extends Mappable<User, UserWithoutTasksDto> {
+public abstract class UserMapper implements Mappable<User, UserWithoutTasksDto> {
 
+    @Autowired
+    protected TaskMapper taskMapper;
+
+    // User -> UserWithoutTasksDto
+    @Override
+    @Mapping(target = "firstName", source = "firstName")
+    @Mapping(target = "lastName", source = "lastName")
+    public abstract UserWithoutTasksDto toDto(User entity);
+
+    // User -> UserWithTasksDto
+    @Mapping(target = "firstName", source = "firstName")
+    @Mapping(target = "lastName", source = "lastName")
+    @Mapping(target = "tasks", expression = "java(mapTasksToDto(user.getTasks()))")
+    public abstract UserWithTasksDto toDtoWithTasks(User user);
+
+    // UserWithoutTasksDto -> User
+    @Override
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "enabled", constant = "false")
+    @Mapping(target = "createdTime", ignore = true)
+    @Mapping(target = "updatedTime", ignore = true)
+    @Mapping(target = "tasks", ignore = true)
+    @Mapping(target = "authorities", ignore = true)
+    public abstract User toEntity(UserWithoutTasksDto dto);
+
+    // Вспомогательный метод для маппинга Set<Task> -> Set<TaskDto>
+    protected Set<TaskDto> mapTasksToDto(Set<Task> tasks) {
+        if (tasks == null) return null;
+        return tasks.stream()
+                .map(taskMapper::toDto)
+                .collect(Collectors.toSet());
+    }
 }
