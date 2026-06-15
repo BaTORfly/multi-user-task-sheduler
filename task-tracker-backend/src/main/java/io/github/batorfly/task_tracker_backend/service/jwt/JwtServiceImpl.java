416a72ecd7efee2cd55b0c7af3f1b2139f0f5b32
Biliktuev Bator <batojjk@gmail.com>
2026-06-15 10:42:19 +0300

    JwtService version 1.0

--- task-tracker-backend/src/main/java/io/github/batorfly/task_tracker_backend/service/jwt/JwtServiceImpl.java
+++ task-tracker-backend/src/main/java/io/github/batorfly/task_tracker_backend/service/jwt/JwtServiceImpl.java
@@ -3,18 +3,23 @@ package io.github.batorfly.task_tracker_backend.service.jwt;
 import io.github.batorfly.task_tracker_backend.domain.user.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.Optional;
+import java.util.function.Function;
 
+/**
+ * JwtService version 1.0
+ */
 @Service
 @Slf4j
 public class JwtServiceImpl implements JwtService {
@@ -39,26 +44,63 @@ public class JwtServiceImpl implements JwtService {
                 .add("id", user.getId())
                 .add("roles", user.getAuthorities())
                 .build();
-        
+
+        Instant validity = Instant.now()
+                .plus(this.accessLifetime, ChronoUnit.SECONDS);
+
+        return Jwts.builder()
+                .claims(claims)
+                .expiration(Date.from(validity))
+                .signWith(signingKey)
+                .compact();
     }
 
     @Override
     public String generateRefreshToken(User user) {
-        return "";
+        Claims claims = Jwts.claims()
+                .subject(user.getEmail())
+                .add("id", user.getId())
+                .build();
+
+        Instant validity = Instant.now()
+                .plus(this.refreshLifetime, ChronoUnit.SECONDS);
+
+        return Jwts.builder()
+                .claims(claims)
+                .expiration(Date.from(validity))
+                .signWith(signingKey)
+                .compact();
     }
 
     @Override
     public Optional<String> getSubjectFromToken(String token) {
-        return Optional.empty();
+        return Optional.ofNullable(getClaim(token, Claims::getSubject));
     }
 
     @Override
     public Date getExpirationDateFromToken(String token) {
-        return null;
+        return getClaim(token, Claims::getExpiration);
     }
 
     @Override
-    public String verifyToken(String token) {
-        return "";
+    public String verifyToken(String refreshToken) {
+        return getSubjectFromToken(refreshToken)
+                .orElseThrow(() -> {
+                    log.error("Token does not have subject (user email)");
+                    return new MalformedJwtException("Token does not have subject (user email)");
+                });
+    }
+
+    private <T> T getClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(signingKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 }
