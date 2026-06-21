d16f413cc0e8084800ac6e85b1101136ef1f04aa
Biliktuev Bator <batojjk@gmail.com>
2026-06-21 18:59:08 +0300

    add open api config to git

---
+++ task-tracker-backend/src/main/java/io/github/batorfly/task_tracker_backend/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package io.github.batorfly.task_tracker_backend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Task Tracker Backend API",
+                version = "1",
+                description = "This application provides JWT-based authentication and the REST API for managing users and tasks."
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
